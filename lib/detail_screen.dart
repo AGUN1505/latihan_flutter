a9ddb083accbc728274bd067ced0289b260f18f7
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2026-01-16 14:36:17 +0700

    feat: penambahan foto dan font

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+var informationTextStyle = const TextStyle(fontFamily: 'Oxygen');
+
 class DetailScreen extends StatelessWidget {
   const DetailScreen({Key? key}) : super(key: key);
 
@@ -7,59 +9,89 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.only(top: 16.0),
-              child: const Text(
-                'Farm House Lembang',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Image.asset('images/farm-house.jpg'),
+              Container(
+                margin: const EdgeInsets.only(top: 16.0),
+                child: const Text(
+                  'Farm House Lembang',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontFamily: 'Staatliches',
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            Container(
-                margin: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              Container(
+                  margin: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          const Icon(Icons.calendar_today),
+                          const SizedBox(height: 8.0),
+                          Text(
+                            'Open Everyday',
+                            style: informationTextStyle,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          const Icon(Icons.access_time),
+                          const SizedBox(height: 8.0),
+                          Text('09:00 - 20:00', style: informationTextStyle),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          const Icon(Icons.monetization_on),
+                          const SizedBox(height: 8.0),
+                          Text('Rp 25.000', style: informationTextStyle),
+                        ],
+                      ),
+                    ],
+                  )),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: const Text(
+                  'Farm House Lembang adalah sebuah tempat wisata yang menawarkan suasana pedesaan Eropa dengan berbagai atraksi menarik.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 150,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
                   children: <Widget>[
-                    Column(
-                      children: const <Widget>[
-                        Icon(Icons.calendar_today),
-                        SizedBox(height: 8.0),
-                        Text('open Everyday'),
-                      ],
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Image.network(
+                          'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
                     ),
-                    Column(
-                      children: const <Widget>[
-                        Icon(Icons.access_time),
-                        SizedBox(height: 8.0),
-                        Text('09:00 - 20:00'),
-                      ],
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Image.network(
+                          'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
                     ),
-                    Column(
-                      children: const <Widget>[
-                        Icon(Icons.monetization_on),
-                        SizedBox(height: 8.0),
-                        Text('Rp 25.000'),
-                      ],
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Image.network(
+                          'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
                     ),
                   ],
-                )),
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: const Text(
-                'Farm House Lembang adalah sebuah tempat wisata yang menawarkan suasana pedesaan Eropa dengan berbagai atraksi menarik.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16.0,
                 ),
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
